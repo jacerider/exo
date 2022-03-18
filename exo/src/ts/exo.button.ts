5e351bded129b207283e5fdd3acbc69e8c456bb3
JaceRider <jacerider@gmail.com>
2022-03-18 10:07:32 -0500

    Apply generically.

--- exo/src/ts/exo.button.ts
+++ exo/src/ts/exo.button.ts
@@ -11,7 +11,7 @@
     attach: function (context) {
       $('.exo-button-trigger', context).once('exo-button').on('click', function (e) {
         e.preventDefault();
-        $(this).closest('.exo-button').find('input[type="submit"]').trigger('mousedown').trigger('mouseup').trigger('click');
+        $(this).closest('.exo-button').find('.js-form-submit').trigger('mousedown').trigger('mouseup').trigger('click');
       });
     }
   };
