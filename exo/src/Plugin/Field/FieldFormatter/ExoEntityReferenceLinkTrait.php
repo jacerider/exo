a2ef2b4825f7815116ae2155a1f17bf5f8570082
JaceRider <jacerider@gmail.com>
2022-03-18 10:07:44 -0500

    Provide helper method to get URL.

--- exo/src/Plugin/Field/FieldFormatter/ExoEntityReferenceLinkTrait.php
+++ exo/src/Plugin/Field/FieldFormatter/ExoEntityReferenceLinkTrait.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\exo\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\FileInterface;
 
 /**
  * Provides a trait for selecting which entities to view.
@@ -33,13 +35,11 @@ trait ExoEntityReferenceLinkTrait {
    */
   public function linkSettingsForm(array &$form, FormStateInterface $form_state) {
     $elements = [];
-
     if (isset($form['image_link'])) {
       if ($options = $this->getLinkFieldOptions()) {
         $form['image_link']['#options'] += $options;
       }
     }
-
     return $elements;
   }
 
@@ -55,6 +55,33 @@ trait ExoEntityReferenceLinkTrait {
       }
     }
     return $options;
+  }
+
+  /**
+   * Get the URL for the file.
+   *
+   * @param \Drupal\file\FileInterface $file
+   *   The entity.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity that the field belongs to.
+   *
+   * @return \Drupal\Core\Url|null
+   *   The URL object for the file item or null if we don't want to add
+   *   a link.
+   */
+  protected function getLinkUrl(FileInterface $file, EntityInterface $entity) {
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      if (!$entity->isNew()) {
+        $url = $entity->toUrl();
+      }
+    }
+    elseif ($image_link_setting === 'file') {
+      $url = $file->toUrl();
+    }
+    return $url;
   }
 
 }
