39c2800fa25e3e216f29b3906cb0d845e0c1703e
JaceRider <jacerider@gmail.com>
2022-05-12 15:44:45 -0500

    Generate extra field render directly.

--- exo_alchemist/src/Plugin/ExoComponentField/EntityExtraField.php
+++ exo_alchemist/src/Plugin/ExoComponentField/EntityExtraField.php
@@ -4,10 +4,12 @@ namespace Drupal\exo_alchemist\Plugin\ExoComponentField;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContextAwarePluginInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Plugin\Context\EntityContext;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\exo_alchemist\Plugin\ExoComponentFieldComputedBase;
@@ -62,9 +64,12 @@ class EntityExtraField extends ExoComponentFieldComputedBase implements ContextA
     $this->entityTypeManager = $entity_type_manager;
     $this->entityFieldManager = $entity_field_manager;
     // Get field name from the plugin ID.
-    list (, , , $field_name) = explode(static::DERIVATIVE_SEPARATOR, $plugin_id, 4);
-    if (empty($field_name)) {
-      list (, , $field_name) = explode(static::DERIVATIVE_SEPARATOR, $plugin_id, 3);
+    $parts = explode(static::DERIVATIVE_SEPARATOR, $plugin_id);
+    if (count($parts) === 3) {
+      $field_name = $parts[2];
+    }
+    if (count($parts) === 4) {
+      $field_name = $parts[3];
     }
     assert(!empty($field_name));
     $this->fieldName = $field_name;
@@ -98,6 +103,27 @@ class EntityExtraField extends ExoComponentFieldComputedBase implements ContextA
   /**
    * {@inheritdoc}
    */
+  public function view(ContentEntityInterface $entity, array $contexts) {
+    if ($this->isPreview($contexts)) {
+      $parts = explode(static::DERIVATIVE_SEPARATOR, $this->pluginId);
+      $entity_type_id = $parts[1];
+      if (count($parts) === 3) {
+        $entity_type_bundle_info = \Drupal::service('entity_type.bundle.info');
+        $bundles = $entity_type_bundle_info->getBundleInfo($entity_type_id);
+        $bundle = key($bundles);
+      }
+      if (count($parts) === 4) {
+        $bundle = $parts[2];
+      }
+      $sample_entity = \Drupal::service('layout_builder.sample_entity_generator')->get($entity_type_id, $bundle);
+      $this->setContext('entity', EntityContext::fromEntity($sample_entity));
+    }
+    return parent::view($entity, $contexts);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function viewValue(ContentEntityInterface $entity, array $contexts) {
     $parent_entity = $this->getParentEntity();
     // Add a placeholder to replace after the entity view is built.
@@ -107,15 +133,25 @@ class EntityExtraField extends ExoComponentFieldComputedBase implements ContextA
       $build = [];
     }
     else {
-      $build = [
-        '#extra_field_placeholder_field_name' => $this->fieldName,
-        // Always provide a placeholder. The Layout Builder will NOT invoke
-        // hook_entity_view_alter() so extra fields will not be added to the
-        // render array. If the hook is invoked the placeholder will be
-        // replaced.
-        // @see ::replaceFieldPlaceholder()
-        '#markup' => $this->t('Placeholder for the @preview_fallback', ['@preview_fallback' => $this->getPreviewFallbackString()]),
-      ];
+      // Render just the extra field. The only way to do this is to fully render
+      // the field.
+      /** @var \Drupal\Core\Entity\EntityViewBuilder $entity_view_builder */
+      $view_mode = 'extra_field';
+      $displays = EntityViewDisplay::collectRenderDisplays([$parent_entity], $view_mode);
+      $display = reset($displays);
+      foreach ($display->getComponents() as $name => $component) {
+        $display->removeComponent($name);
+      }
+      $display->setComponent($this->fieldName);
+      $view_hook = "{$parent_entity->getEntityTypeId()}_view";
+      $module_handler = \Drupal::moduleHandler();
+      $entity_build = [];
+      $module_handler->invokeAll($view_hook, [&$entity_build, $parent_entity, $display, $view_mode]);
+      $module_handler->invokeAll('entity_view', [&$entity_build, $entity, $display, $view_mode]);
+      $build = [];
+      if (isset($entity_build[$this->fieldName])) {
+        $build = $entity_build[$this->fieldName];
+      }
     }
     CacheableMetadata::createFromObject($this)->applyTo($build);
     return [
