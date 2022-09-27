6cecf4cda7ee99743d4a9a41503b2fbc220eb932
JaceRider <jacerider@gmail.com>
2022-09-27 09:17:14 -0500

    Allow totals to be shown with links.

--- exo_list_builder/src/Plugin/ExoList/Widget/Links.php
+++ exo_list_builder/src/Plugin/ExoList/Widget/Links.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\exo_list_builder\Plugin\ExoList\Widget;
 
+use Drupal\Component\Utility\SortArray;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\exo_icon\ExoIconTranslationTrait;
 use Drupal\exo_list_builder\EntityListInterface;
@@ -28,6 +29,7 @@ class Links extends ExoListWidgetBase implements ExoListWidgetValuesInterface {
   public function defaultConfiguration() {
     return [
       'group' => NULL,
+      'total' => FALSE,
     ] + parent::defaultConfiguration();
   }
 
@@ -37,6 +39,11 @@ class Links extends ExoListWidgetBase implements ExoListWidgetValuesInterface {
   public function buildConfigurationForm(array $form, FormStateInterface $form_state, EntityListInterface $entity_list, ExoListFilterInterface $filter, array $field) {
     $form = parent::buildConfigurationForm($form, $form_state, $entity_list, $filter, $field);
     $configuration = $this->getConfiguration();
+    $form['total'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show total in link'),
+      '#default_value' => $configuration['total'],
+    ];
     if ($filter instanceof ExoListFieldPropertyInterface) {
       $properties = ['' => $this->t('- None -')] + $filter->getPropertyOptions($field['definition']);
       if (count($properties) > 2) {
@@ -65,6 +72,7 @@ class Links extends ExoListWidgetBase implements ExoListWidgetValuesInterface {
     $options = $filter->getFilteredValueOptions($entity_list, $field);
     $multiple = !empty($field['filter']['settings']['multiple']);
     $current = $entity_list->getHandler()->getOption(['filter', $field['id']]) ?? [];
+    $total = $entity_list->getHandler()->getTotal();
     $current = is_array($current) ? $current : [$current];
     $items = [];
     if (!empty($configuration['group'])) {
@@ -76,64 +84,69 @@ class Links extends ExoListWidgetBase implements ExoListWidgetValuesInterface {
       $groups = array_filter($groups);
       foreach ($groups as $group => $values) {
         asort($values);
-        if ($groupItems = $this->buildLinks($entity_list, $field, $values, $current, $multiple)) {
+        if ($groupItems = $this->buildLinks($entity_list, $filter, $field, $values, $current, $multiple, $total)) {
           $list = [
             '#theme' => 'item_list',
             '#title' => $group,
             '#items' => $groupItems,
           ];
-
-          if (count($groups) > 1) {
-            $items[] = $list;
-          }
+          $items[] = $list;
         }
       }
     }
     else {
-      $items = $this->buildLinks($entity_list, $field, array_keys($options), $current, $multiple);
+      $items = $this->buildLinks($entity_list, $filter, $field, array_keys($options), $current, $multiple, $total);
     }
     $element = [
       '#theme' => 'item_list',
       '#title' => $element['#title'],
       '#items' => $items,
+      '#access' => !empty($items),
+      '#prefix' => '<div class="exo-form-element exo-form-element-type-links">',
+      '#suffix' => '</div>',
     ];
   }
 
   /**
    * Build links.
    */
-  protected function buildLinks(EntityListInterface $entity_list, array $field, $values, $current, $multiple) {
+  protected function buildLinks(EntityListInterface $entity_list, ExoListFilterInterface $filter, array $field, $values, $current, $multiple, $total) {
     $items = [];
-    $has_current = FALSE;
+    $configuration = $this->getConfiguration();
+    // $value_current = $entity_list->getHandler()->getOption(['filter', $field['id']]);
     foreach ($values as $value) {
+      $value_total = !empty($configuration['total']) ? $filter->getOptionTotal(array_unique(array_merge($current, [$value])), $entity_list, $field) : 0;
       $is_current = in_array($value, $current);
       $url_value = $multiple ? array_merge($current, [$value]) : $value;
       if ($is_current) {
         $url_value = $multiple ? array_filter($current, function ($item) use ($value) {
           return $item != $value;
         }) : $value;
-        $has_current = TRUE;
       }
       $filters = !empty($url_value) ? [
         $field['id'] => $url_value,
       ] : [];
+      if (!$is_current && $value_total && $value_total === $total) {
+        continue;
+      }
       $items[] = [
         '#type' => 'link',
-        '#title' => $value,
+        '#value' => $value,
         '#title' => [
           '#type' => 'inline_template',
-          '#template' => '<span class="value">{{ value }}</span>{% if remove %} <span class="remove">{{ remove }}</span>{% endif %}',
+          '#template' => '<span class="value">{{ value }}{% if total %} <span class="total">({{ total }})</span>{% endif %}</span>{% if remove %} <span class="remove">{{ remove }}</span>{% endif %}',
           '#context' => [
             'value' => $value,
             'remove' => !$is_current ? NULL : $this->icon('Remove')->setIcon('regular-times')->setIconOnly(),
+            'total' => $value_total,
           ],
         ],
         '#url' => $entity_list->toFilteredUrl($filters),
       ];
     }
-    if (!$has_current && count($items) <= 1) {
-      return [];
-    }
+    uasort($items, function ($a, $b) {
+      return strnatcmp($a['#value'], $b['#value']);
+    });
     return $items;
   }
 
