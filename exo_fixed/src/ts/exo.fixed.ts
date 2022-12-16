9de330aa44132f26dae258f956bf2bc093ff1b6f
JaceRider <jacerider@gmail.com>
2022-12-16 09:02:15 -0600

    Improve fixed calculations.

--- exo_fixed/src/ts/exo.fixed.ts
+++ exo_fixed/src/ts/exo.fixed.ts
@@ -100,13 +100,13 @@
       this.floatStart = this.floatStart >= 0 ? this.floatStart : 0;
       // Settings to -1 means it will continue to be floated. This will only
       // apply to items that are flush to the top.
-      this.floatEnd = this.floatStart === 0 ? -1 : this.floatStart;
+      this.floatEnd = this.floatStart <= 0 ? 1 : this.floatStart;
       this.themeStart = Math.round(this.floatStart + this.height);
-      this.themeEnd = Math.round(this.floatEnd + this.height);
+      this.themeEnd = Math.round(this.floatEnd + 1);
       if (this.type === 'scroll') {
-        this.floatStart = this.floatEnd + this.height;
+        this.floatStart = Math.round(this.floatEnd + this.height);
         this.themeStart = this.floatStart;
-        this.themeEnd = this.floatStart + this.height;
+        this.themeEnd = Math.round(this.floatStart + 1);
       }
     }
 
