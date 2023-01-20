67f669f7b3175744169bb2633fdb74d7bbfde93c
JaceRider <jacerider@gmail.com>
2023-01-20 12:40:19 -0600

    Rework the way first/last component classes are added to wrapper.

---
+++ exo_alchemist/src/ts/exo.alchemist.ts
@@ -0,0 +1,13 @@
+const page = document.querySelectorAll('.section.page');
+if (page.length) {
+  ['first', 'last'].forEach(position => {
+    // Select only the first level of elements.
+    const elements = document.querySelectorAll('.alchemist.node.full > .layout > .layout__region > .exo-component-wrapper[data-component-' + position + ']');
+    if (elements.length) {
+      const id = elements[0].getAttribute('data-component-' + position).replace(/_/g, '-');
+      console.log(id);
+      page[0].classList.add('component-enabled');
+      page[0].classList.add('component-' + position + '--' + id);
+    }
+  });
+}
