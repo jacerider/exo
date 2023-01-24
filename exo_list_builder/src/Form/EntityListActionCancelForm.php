1375c412a7bd6e6cd0035bf8d2dd2f688c1f7b3e
JaceRider <jacerider@gmail.com>
2023-01-24 13:31:22 -0600

    Do not use entity form for config lock.

--- exo_list_builder/src/Form/EntityListActionCancelForm.php
+++ exo_list_builder/src/Form/EntityListActionCancelForm.php
@@ -3,13 +3,28 @@
 namespace Drupal\exo_list_builder\Form;
 
 use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Provides an entities deletion confirmation form.
  */
-class EntityListActionCancelForm extends EntityConfirmFormBase {
+class EntityListActionCancelForm extends ConfirmFormBase {
+
+  /**
+   * The name of the current operation.
+   *
+   * Subclasses may use this to implement different behaviors depending on its
+   * value.
+   *
+   * @var string
+   */
+  protected $operation;
 
   /**
    * The entity being used by this form.
@@ -26,6 +41,27 @@ class EntityListActionCancelForm extends EntityConfirmFormBase {
   protected $action;
 
   /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'entity_list_action_cancel_form';
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state, $exo_entity_list_action = NULL) {
@@ -36,6 +72,13 @@ class EntityListActionCancelForm extends EntityConfirmFormBase {
     $this->action = $actions[$exo_entity_list_action];
     $form = parent::buildForm($form, $form_state);
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return $this->t('This action cannot be undone.<br>');
   }
 
   /**
@@ -102,6 +145,74 @@ class EntityListActionCancelForm extends EntityConfirmFormBase {
     $this->messenger()->addMessage($message);
 
     $form_state->setRedirectUrl($this->getCancelUrl());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntity() {
+    return $this->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setEntity(EntityInterface $entity) {
+    $this->entity = $entity;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEntityFromRouteMatch(RouteMatchInterface $route_match, $entity_type_id) {
+    if ($route_match->getRawParameter($entity_type_id) !== NULL) {
+      $entity = $route_match->getParameter($entity_type_id);
+    }
+    else {
+      $values = [];
+      // If the entity has bundles, fetch it from the route match.
+      $entity_type = $this->entityTypeManager->getDefinition($entity_type_id);
+      if ($bundle_key = $entity_type->getKey('bundle')) {
+        if (($bundle_entity_type_id = $entity_type->getBundleEntityType()) && $route_match->getRawParameter($bundle_entity_type_id)) {
+          $values[$bundle_key] = $route_match->getParameter($bundle_entity_type_id)->id();
+        }
+        elseif ($route_match->getRawParameter($bundle_key)) {
+          $values[$bundle_key] = $route_match->getParameter($bundle_key);
+        }
+      }
+
+      $entity = $this->entityTypeManager->getStorage($entity_type_id)->create($values);
+    }
+
+    return $entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOperation($operation) {
+    // If NULL is passed, do not overwrite the operation.
+    if ($operation) {
+      $this->operation = $operation;
+    }
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setModuleHandler(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setEntityTypeManager(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+    return $this;
   }
 
 }
