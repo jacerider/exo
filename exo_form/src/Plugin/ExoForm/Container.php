15d7a88d606b33bc5dae80fbcbde3c57bfaf91e6
JaceRider <jacerider@gmail.com>
2023-05-18 10:11:13 -0500

    Attempt to fix ajax issue where empty containers used for ajax were hidden.

--- exo_form/src/Plugin/ExoForm/Container.php
+++ exo_form/src/Plugin/ExoForm/Container.php
@@ -39,7 +39,7 @@ class Container extends ExoFormBase {
    */
   public function preRender($element) {
     // If we have no visible children, we hide the container.
-    if (!Element::getVisibleChildren($element)) {
+    if (!Element::getVisibleChildren($element) && empty($element['#id']) && empty($element['#attributes']['id'])) {
       return [];
     }
     $element['#exo_form_element_attributes']['class'][] = 'exo-form-element';
