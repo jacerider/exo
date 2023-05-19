96852fccca701116b7e9bb9710d9ad1de0d950fd
JaceRider <jacerider@gmail.com>
2023-05-19 13:27:12 -0500

    Better support for editing swiper slides when all are “visible”.

--- exo_alchemist/src/ts/exo.alchemist.enhancement.slider.ts
+++ exo_alchemist/src/ts/exo.alchemist.enhancement.slider.ts
@@ -101,6 +101,9 @@
 
       $(document).on('exoComponentFieldEditActive.exo.alchemist.enhancement.slider.' + this.id, (e, element) => {
         let $element = $(element);
+        if ($(this.swiper.visibleSlides).data('exo-field')) {
+          $element = $(this.swiper.visibleSlides);
+        }
         if (this.$wrapper.find($element).length) {
           this.swiper.slideTo($element.index());
           this.swiper.once('slideChangeTransitionEnd', function () {
