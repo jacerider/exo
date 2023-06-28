e72c12a8d08f5836de2cfe1c3e3965cb8b618b8a
JaceRider <jacerider@gmail.com>
2023-06-28 09:28:24 -0500

    Drush <= 11 support.

--- exo/src/Drush11/Generators/ExoThemeGenerator.php
+++ exo/src/Drush11/Generators/ExoThemeGenerator.php
@@ -1,35 +1,18 @@
 <?php
 
-namespace Drupal\exo\Drush\Generators;
+namespace Drupal\exo\Drush11\Generators;
 
 use Drupal\Core\Extension\ModuleExtensionList;
 use Drupal\exo\ExoThemeProviderPluginManagerInterface;
-use DrupalCodeGenerator\Application;
+use DrupalCodeGenerator\Command\ModuleGenerator;
 use DrupalCodeGenerator\Utils;
-use DrupalCodeGenerator\Attribute\Generator;
-use DrupalCodeGenerator\GeneratorType;
-use DrupalCodeGenerator\Command\BaseGenerator;
-use DrupalCodeGenerator\Asset\AssetCollection;
-use DrupalCodeGenerator\Helper\Renderer\TwigRenderer;
-use DrupalCodeGenerator\Twig\TwigEnvironment;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Twig\Loader\FilesystemLoader as TemplateLoader;
+use Symfony\Component\Console\Question\Question;
+use DrupalCodeGenerator\Asset\File;
 
-// Drush <11 compatibility.
-if (!class_exists('DrupalCodeGenerator\Command\BaseGenerator')) {
-  return;
-}
-
-#[Generator(
-  name: 'exo:theme',
-  description: 'Generates eXo theme module',
-  templatePath: __DIR__ . '/ExoTheme',
-  type: GeneratorType::MODULE_COMPONENT,
-)]
 /**
- * Exo theme generator.
+ * The exo theme generator.
  */
-final class ExoThemeGenerator extends BaseGenerator {
+class ExoThemeGenerator extends ModuleGenerator {
 
   /**
    * {@inheritdoc}
@@ -75,13 +58,6 @@ final class ExoThemeGenerator extends BaseGenerator {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container): self {
-    return new self($container->get('extension.list.module'), $container->get('plugin.manager.exo_theme_provider'));
-  }
-
-  /**
    * An array of color options.
    */
   protected function getColorOptions() {
@@ -96,25 +72,38 @@ final class ExoThemeGenerator extends BaseGenerator {
   /**
    * {@inheritdoc}
    */
-  protected function generate(array &$vars, AssetCollection $assets): void {
-    $ir = $this->createInterviewer($vars);
-    $vars['machine_name'] = $ir->askMachineName();
-    $vars['class'] = $ir->askClass(default: '{machine_name|camelize}Theme');
-    $vars['label'] = $ir->ask('Theme label', '{machine_name|m2h} Theme');
-    $vars['plugin_id'] = $ir->ask('Plugin ID', '{machine_name}');
+  protected function generate(&$vars): void {
+    $this->collectDefault($vars);
+
+    $vars['module'] = $vars['machine_name'];
+
+    $default = Utils::camelize($vars['machine_name'] . 'Theme');
+    $question = new Question('Theme class name', $default);
+    $question->setValidator([static::class, 'validateRequired']);
+    $vars['class'] = $this->io->askQuestion($question);
+
+    $default = Utils::camelize($vars['machine_name']);
+    $question = new Question('Theme label', $default);
+    $question->setValidator([static::class, 'validateRequired']);
+    $vars['label'] = $this->io->askQuestion($question);
+
+    $default = $vars['machine_name'];
+    $question = new Question('Plugin ID', $default);
+    $question->setValidator([static::class, 'validateRequired']);
+    $vars['plugin_id'] = Utils::human2machine($this->io->askQuestion($question));
     $vars['plugin_id_camel_upper'] = ucfirst(Utils::camelize(Utils::machine2human($vars['plugin_id'])));
 
     foreach ($this->getColorOptions() as $key => $value) {
-      $vars['colors'][$key] = $ir->ask(ucfirst($key) . ' Color ', $value);
+      $vars['colors'][$key] = $this->ask(ucfirst($key) . ' Color ', $value);
     }
 
-    $assets->addFile('src/ExoTheme/{plugin_id_camel_upper}/{class}.php', 'exo-theme.php.twig');
-    $assets->addFile('src/ExoTheme/{plugin_id_camel_upper}/scss/_exo-theme.scss', '_exo-theme.scss.twig');
+    $this->addFile('src/ExoTheme/{plugin_id_camel_upper}/{class}.php', 'exo-theme.php.twig');
+    $this->addFile('src/ExoTheme/{plugin_id_camel_upper}/scss/_exo-theme.scss', '_exo-theme.scss.twig');
 
-    $this->getHelper('renderer')->registerTemplatePath(__DIR__ . '/GulpScss');
-    $assets->addFile('.gitignore', '.gitignore.twig');
-    $assets->addFile('gulpfile.js', 'gulpfile.js.twig');
-    $assets->addFile('package.json', 'package.json.twig');
+    $this->getHelper('renderer')->prependPath(__DIR__ . '/GulpScss');
+    $this->addFile('.gitignore', '.gitignore.twig');
+    $this->addFile('gulpfile.js', 'gulpfile.js.twig');
+    $this->addFile('package.json', 'package.json.twig');
 
     // Prepare includes.
     $vars['includes'] = $this->getThemeIncludes();
@@ -123,21 +112,13 @@ final class ExoThemeGenerator extends BaseGenerator {
     $plugin_path = $module_path . '/src/ExoTheme/' . $vars['plugin_id_camel_upper'];
     $plugin_asset_path = $plugin_path . '/scss';
     foreach ($this->exoThemeProviderManager->getAllDefinitions() as $provider_plugin_id => $definition) {
-      $collection = new AssetCollection();
-      $template_loader = new TemplateLoader();
-      $template_loader->addPath(Application::TEMPLATE_PATH . '/_lib', 'lib');
-      $renderer = new TwigRenderer(new TwigEnvironment($template_loader));
-      $renderer->setLogger($this->logger);
-      $renderer->registerTemplatePath($definition['providerPath'] . '/src/ExoThemeProvider');
+      $this->getHelper('renderer')->prependPath($definition['providerPath'] . '/src/ExoThemeProvider');
       $vars['theme_relative_path'] = $this->getRelativePath($plugin_asset_path, $definition['providerPath']);
-      $collection->addFile('src/ExoTheme/{plugin_id_camel_upper}/scss/' . str_replace('_', '-', $definition['id']) . '.scss', $definition['template'])->vars($vars);
-      foreach ($collection->getFiles() as $file) {
-        /** @var \DrupalCodeGenerator\Asset\File $file */
-        $file->render($renderer);
-      }
-      $destination = $this->getDestination($vars);
-      $dumped_assets = $this->dump($collection, $destination);
-      $this->printSummary($dumped_assets, $destination . '/');
+      $asset = new File('src/ExoTheme/{plugin_id_camel_upper}/scss/' . str_replace('_', '-', $definition['id']) . '.scss');
+      $asset->template($definition['template']);
+      $asset->vars($vars);
+      $this->assets[] = $asset;
+      // $this->addFile('src/ExoTheme/{plugin_id_camel_upper}/scss/' . str_replace('_', '-', $definition['id']) . '.scss', $definition['template']);
     }
   }
 
