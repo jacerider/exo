1b56bc0ab57c2a7b0ad979c9eb664e34761ba170
JaceRider <jacerider@gmail.com>
2024-03-27 09:47:05 -0500

    Optimize.

--- exo_alchemist/src/Plugin/Validation/Constraint/ExoComponentTagValidator.php
+++ exo_alchemist/src/Plugin/Validation/Constraint/ExoComponentTagValidator.php
@@ -14,9 +14,11 @@ class ExoComponentTagValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($items, Constraint $constraint) {
+    /** @var \Drupal\exo_alchemist\Plugin\Validation\Constraint\ExoComponentTag $constraint */
     $valid = FALSE;
+    $tags = $constraint->getTags();
     foreach ($items as $tag) {
-      foreach ($constraint->getTags() as $definition_tag) {
+      foreach ($tags as $definition_tag) {
         if ($tag === $definition_tag) {
           $valid = TRUE;
         }
