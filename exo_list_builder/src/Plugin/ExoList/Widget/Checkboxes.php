91b7b90077e33915ebf451fe7d5eebcec5f0436a
Jacob Weber <jacobaweber@gmail.com>
2024-04-02 14:26:15 -0500

    Adding a checkboxes widget to exo list builder

---
+++ exo_list_builder/src/Plugin/ExoList/Widget/Checkboxes.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\exo_list_builder\Plugin\ExoList\Widget;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\exo_list_builder\EntityListInterface;
+use Drupal\exo_list_builder\Plugin\ExoListFilterInterface;
+use Drupal\exo_list_builder\Plugin\ExoListWidgetBase;
+use Drupal\exo_list_builder\Plugin\ExoListWidgetValuesInterface;
+
+/**
+ * Defines a eXo list action for batch operations.
+ *
+ * @ExoListWidget(
+ *   id = "checkboxes",
+ *   label = @Translation("Checkboxes"),
+ *   description = @Translation("Checkboxes widget."),
+ * )
+ */
+class Checkboxes extends ExoListWidgetBase implements ExoListWidgetValuesInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'limit' => 50,
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state, EntityListInterface $entity_list, ExoListFilterInterface $filter, array $field) {
+    $form = parent::buildConfigurationForm($form, $form_state, $entity_list, $filter, $field);
+    $configuration = $this->getConfiguration();
+    $form['limit'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Maximum Results'),
+      '#default_value' => $configuration['limit'],
+      '#required' => TRUE,
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function alterElement(array &$element, EntityListInterface $entity_list, ExoListFilterInterface $filter, array $field) {
+    $configuration = $this->getConfiguration();
+    $options = $filter->getFilteredValueOptions($entity_list, $field);
+    $element['#type'] = 'checkboxes';
+    $element['#title'] = $filter->getConfiguration()['label'];
+    $element['#options'] = $options;
+    $element['#multiple'] = $filter->allowsMultiple($field);
+    $element['#default_value'] = $options;
+    $element['#element_validate'] = [[get_class($this), 'validateElement']];
+  }
+
+  /**
+   * Element validate callback.
+   */
+  public static function validateElement($element, FormStateInterface $form_state) {
+    $value = array_filter($form_state->getValue($element['#parents']));
+    if ($value && isset($element['#exo_configuration']['empty_value']) && $element['#exo_configuration']['empty_value'] === $value) {
+      $value = NULL;
+    }
+    $form_state->setValue($element['#parents'], $value);
+  }
+
+}
